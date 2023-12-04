9e847a7eeec97d21feeaa203bfb1a7c3b0d35814
azzky <azzky-demiurg@mail.ru>
2023-12-04 13:28:09 +0100

    added manifest, robots and sitemap

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/static/manifest.json" />
-        <link rel="manifest" href="/static/site.webmanifest" />
-        <link rel="shortcut icon" href="/static/favicon-32x32.png"/>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon-32x32.png"/>
       </Head>
       <body>
         <Main />
