30bb3f2b2f2064b3bdc38ffac9c1da897848acaa
azzky <azzky-demiurg@mail.ru>
2024-04-04 12:25:16 +0200

    lang switcher update

--- src/components/lang-switcher/index.jsx
+++ src/components/lang-switcher/index.jsx
@@ -7,18 +7,19 @@ import * as classes from './langSwitcher.module.scss';
 
 const Switcher = () => {
     const router = useRouter();
+    const otherLang = router.locale === 'ru' ? 'en' : 'ru';
 
     const handler = useCallback(() => {
         const path = router.asPath;
-        router.push(path, path, { locale: router.locale === 'ru' ? 'en' : 'ru' });
-    }, [router]);
+        router.push(path, path, { locale: otherLang });
+    }, [router, otherLang]);
 
     return (
         <>
             <div className={classes.root}>
                 <button onClick={handler}
                     className={classes.button}>
-                    en/ru
+                    {otherLang}
                 </button>
             </div>
         </>
